c90fbbc83d0a01343135e41ce32fd97f694042b5
sakals <sakals@mail.ua>
2017-12-18 21:37:45 -0600

    init: project helpers
    Signed-off-by: sakals <sakals@mail.ua>

---
+++ app/utils/helpers.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import {View} from 'react-native';
+import {FontAwesome, MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons';
+import {colorTheme} from './colorTheme';
+
+export function getMetricMetaInfo(metric) {
+  const info = {
+    run: {
+      displayName: 'Run',
+      max: 50,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+            <View>
+              <MaterialIcons
+                  name='directions-run'
+                  color={colorTheme.black}
+                  size={35}
+              />
+            </View>
+        );
+      },
+    },
+    bike: {
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+            <View>
+              <MaterialCommunityIcons
+                  name='bike'
+                  color={colorTheme.black}
+                  size={32}
+              />
+            </View>
+        );
+      },
+    },
+    swim: {
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meters',
+      step: 100,
+      type: 'steppers',
+      getIcon() {
+        return (
+            <View>
+              <MaterialCommunityIcons
+                  name='swim'
+                  color={colorTheme.black}
+                  size={35}
+              />
+            </View>
+        );
+      },
+    },
+    sleep: {
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+            <View>
+              <FontAwesome
+                  name='bed'
+                  color={colorTheme.black}
+                  size={30}
+              />
+            </View>
+        );
+      },
+    },
+    eat: {
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+            <View>
+              <MaterialCommunityIcons
+                  name='food'
+                  color={colorTheme.black}
+                  size={35}
+              />
+            </View>
+        );
+      },
+    },
+  };
+
+  return typeof metric === 'undefined'
+      ? info
+      : info[metric];
+}
+
+export function isBetween(num, x, y) {
+  return num >= x && num <= y;
+}
+
+export function calculateDirection(heading) {
+  let direction = '';
+
+  if (isBetween(heading, 0, 22.5)) {
+    direction = 'North';
+  } else if (isBetween(heading, 22.5, 67.5)) {
+    direction = 'North East';
+  } else if (isBetween(heading, 67.5, 112.5)) {
+    direction = 'East';
+  } else if (isBetween(heading, 112.5, 157.5)) {
+    direction = 'South East';
+  } else if (isBetween(heading, 157.5, 202.5)) {
+    direction = 'South';
+  } else if (isBetween(heading, 202.5, 247.5)) {
+    direction = 'South West';
+  } else if (isBetween(heading, 247.5, 292.5)) {
+    direction = 'West';
+  } else if (isBetween(heading, 292.5, 337.5)) {
+    direction = 'North West';
+  } else if (isBetween(heading, 337.5, 360)) {
+    direction = 'North';
+  } else {
+    direction = 'Calculating';
+  }
+
+  return direction;
+}
+
+export function timeToString(time = Date.now()) {
+  const date = new Date(time);
+  const todayUTC = new Date(
+      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+  return todayUTC.toISOString().split('T')[0];
+}
