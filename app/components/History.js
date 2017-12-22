a63b88e778cf091beb8aa75a0171d6d6e48db3a7
sakals <sakals@mail.ua>
2017-12-21 21:27:24 -0600

    feat: connect calendar add styling
    Signed-off-by: sakals <sakals@mail.ua>

--- app/components/History.js
+++ app/components/History.js
@@ -1,34 +1,89 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import styled from 'styled-components/native';
+import {AppLoading} from 'expo';
+import colorTheme from '../utils/colorTheme';
 import {addEntry, receiveEntries} from '../redux-core/actions/index';
 import {getDailyReminderValue, timeToString} from '../utils/helpers';
 import {fetchCalendarResults} from '../utils/api';
 
-import {Text, View} from 'react-native';
+import {Platform, Text, TouchableOpacity, View} from 'react-native';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
+import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
+
+const Item = styled(View)`
+  backgroundColor: ${colorTheme.white}
+  borderRadius: ${Platform.OS === 'ios' ? 16 : 2};
+  padding: 20px;
+  margin: 17px 10px;
+  justifyContent: center;
+`;
+const NoDataText = styled(Text)`
+  fontSize: 20;
+  padding: 20px;
+`;
 
 @connect(store => ({store}))
 class History extends React.Component {
   dispatch = this.props.dispatch;
 
-  componentDidMount() {
+  state = {ready: false};
 
-    fetchCalendarResults().then(entries => {
-      console.log(33);
-      return this.dispatch(receiveEntries(entries));
-    }).then(({entries}) => {
-      if (!entries[timeToString()]) {
-        this.dispatch(addEntry({
-          [timeToString()]: getDailyReminderValue(),
-        }));
-      }
-    });
+  componentDidMount() {
+    fetchCalendarResults().
+        then((entries) => this.dispatch(receiveEntries(entries))).
+        then((entries) => {
+          if (!entries[timeToString()]) {
+            this.dispatch(addEntry({
+              [timeToString()]: getDailyReminderValue(),
+            }));
+          }
+        }).
+        then(() => this.setState(() => ({ready: true})));
   }
 
-  render() {
+  renderItem = ({today, ...metrics}, formattedDate, key) => (
+      <Item>
+        {today
+            ? <View>
+              <DateHeader date={formattedDate}/>
+              <NoDataText>
+                {today}
+              </NoDataText>
+            </View>
+            : <TouchableOpacity onPress={() => console.log('Pressed!')}>
+              <MetricCard date={formattedDate}
+                          metrics={metrics}
+              />
+            </TouchableOpacity>
+        }
+      </Item>
+  );
+  renderEmptyDate = (formattedDate) => {
     return (
-        <View>
-          <Text>{JSON.stringify(this.props)}</Text>
-        </View>
+        <Item>
+          <DateHeader date={formattedDate}/>
+          <NoDataText>
+            You didn't log any data on this day.
+          </NoDataText>
+        </Item>
+    );
+  };
+
+  render() {
+    const {store} = this.props;
+    const {ready} = this.state;
+
+    if (!ready) {
+      return <AppLoading />;
+    }
+
+    return (
+        <UdaciFitnessCalendar items={store}
+                              renderItem={this.renderItem}
+                              renderEmptyDate={this.renderEmptyDate}
+        />
     );
   }
 }
